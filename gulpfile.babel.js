f122fb89a6ca6ba40391ec0abc0b8c56ca69dd67
vlad-gritsenko <vld.gritsenko@gmail.com>
2017-12-28 13:14:22 +0200

    new version arrived

---
+++ gulpfile.babel.js
@@ -0,0 +1,91 @@
+const   gulp          = require('gulp'),
+        babel         = require('gulp-babel'),
+        sass          = require('gulp-sass'),
+        autoprefixer  = require('gulp-autoprefixer'),
+        browserSync   = require('browser-sync'),
+        concat        = require('gulp-concat'),
+        uglify        = require('gulp-uglifyjs'),
+        cssnano       = require('gulp-cssnano'),
+        rename        = require('gulp-rename'),
+        del           = require('del'),
+        imagemin      = require('gulp-imagemin'),
+        cache         = require('gulp-cache');
+
+gulp.task('sass', () => {
+    return gulp.src('src/sass/**/*.sass')
+        .pipe(sass())
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', () => {
+    browserSync({
+        server: {
+            baseDir: 'src'
+        },
+        notify: false
+    });
+});
+
+gulp.task('libs', () => {
+  return gulp.src('src/libs/**/*.js')
+    .pipe(concat('libs.js'))
+    .pipe(uglify())
+    .pipe(babel())
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('scripts', () => {
+    return gulp.src('src/js/*.js')
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(babel())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('css', ['sass'], () => {
+    return gulp.src('src/css/*.css')
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch', ['browser-sync'], () => {
+    gulp.watch('src/sass/**/*.sass', ['sass']);
+    gulp.watch('src/*.html', browserSync.reload);
+    gulp.watch('src/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('clean', () => {
+    return del.sync('dist');
+});
+
+gulp.task('img', () => {
+    return gulp.src('src/images/**/*')
+        .pipe(cache(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}]
+        })))
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('clear', () => {
+    return cache.clearAll();
+});
+
+gulp.task('build', ['clean', 'sass', 'scripts', 'libs', 'css', 'img'], () => {
+
+    let buildCss = gulp.src([
+        'src/css/main.css',
+        'src/css/libs.min.css'
+        ])
+    .pipe(gulp.dest('dist/css'));
+
+    let buildFonts = gulp.src('src/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'));
+
+    let buildHtml = gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+});
